40d45facb75809edc35a85b0a540f13c5fd89974
Orion Osborn <orion.osborn@gmail.com>
2013-07-15 21:20:54 +0000

    updated web.js again.

--- web.js
+++ web.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
+var file = fs.readFilesSync('index.html');
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(file.toString);
 });
 
 var port = process.env.PORT || 5000;
