b6c93463fdce8fa1581db232741c0c97a606e212
Orion Osborn <orion.osborn@gmail.com>
2013-07-15 22:11:48 +0000

    yet another change to web.js

--- web.js
+++ web.js
@@ -2,10 +2,10 @@ var express = require('express');
 var fs = require('fs');
 
 var app = express.createServer(express.logger());
-var file = fs.readFileSync('index.html');
+var file = fs.readFileSync('./index.html');
 
 app.get('/', function(request, response) {
-  response.send(file.toString);
+  response.send(file.toString());
 });
 
 var port = process.env.PORT || 5000;
