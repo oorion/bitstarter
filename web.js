be1130d45871cf11c5b25f79f5a343389c1dd939
Ubuntu <ubuntu@ip-172-31-22-255.us-west-2.compute.internal>
2013-07-26 01:01:51 +0000

    updated index.html with bootstrap

--- web.js
+++ web.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var fs = require('fs');
 
@@ -8,7 +10,7 @@ app.get('/', function(request, response) {
   response.send(file.toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
